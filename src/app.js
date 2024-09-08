cc3a5f29e8f494c12126fe3da4fa7c237c4ee78c
Gustav Larsson <gl222pp@student.lnu.se>
2024-09-08 11:05:08 +0200

    Import color-picker to app working

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@
  * @version 1.0.0
  */
 
-import { ColorPicker } from "./color-picker"
+import { ColorPicker } from "./Color-picker.js"
+
+const colorPicker = new ColorPicker
+
+console.log(colorPicker.convertColor('red'))
 
 console.log('\x1b[35m%s\x1b[0m','Hello babe')
---
+++ src/color-picker.js
@@ -0,0 +1,22 @@
+/**
+ * The color picking module.
+ *
+ * @author Gustav Larsson <gl222pp@student.lnu.se>
+ * @version 1.0.0
+ */
+
+/**
+ * Represents a color picker.
+ */
+export class ColorPicker {
+  /**
+   * Converts a color description to an escape code.
+   *
+   * @param {string} color - The picked color.
+   * @returns {string} - The ANSI escape code for the given color.
+   */
+  convertColor (color) {
+    return 'Great success'
+  }
+
+}
